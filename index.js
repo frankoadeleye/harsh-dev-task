79184847dff5c716aa243006bbf4a37c551506b3
frankoadeleye <oluwatimiadeleye22@gmail.com>
2024-04-01 17:54:01 +0100

    feat: added accordion section

---
+++ index.js
@@ -0,0 +1,15 @@
+// Accordion Style Element, use class="accordion"
+(() => {
+  const acc = document.getElementsByClassName("accordion"), l = acc.length;
+  for (let i = 0; i < l; i++) {
+      acc[i].firstChild.nextSibling.onclick = function () {
+          this.classList.toggle("active");
+          const panel = this.nextSibling.nextSibling;
+          if (panel.style.maxHeight) {
+              panel.style.maxHeight = null;
+          } else {
+              panel.style.maxHeight = panel.scrollHeight + "px";
+          } 
+      }
+  }
+})();

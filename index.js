7890a8f2ee1e9fa2096447996fb4452fab747a89
frankoadeleye <oluwatimiadeleye22@gmail.com>
2024-04-08 17:06:04 +0100

    feat: added hover buttons for all and validated forms

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+/* __________________________________________________ ACCORDION _______________________________________________ */
 (() => {
-  const acc = document.getElementsByClassName("accordion"), l = acc.length;
+  const acc = document.getElementsByClassName("accordion"),
+    l = acc.length;
   for (let i = 0; i < l; i++) {
-      acc[i].firstChild.nextSibling.onclick = function () {
-          this.classList.toggle("active");
-          const panel = this.nextSibling.nextSibling;
-          if (panel.style.maxHeight) {
-              panel.style.maxHeight = null;
-          } else {
-              panel.style.maxHeight = panel.scrollHeight + "px";
-          } 
+    acc[i].firstChild.nextSibling.onclick = function () {
+      this.classList.toggle("active");
+      const panel = this.nextSibling.nextSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
       }
+    };
   }
 })();
+/* __________________________________________________ ACTIVE-TAB _______________________________________________ */
+function activeTab(itemList) {
+  let acctOptions = document.querySelectorAll(itemList);
+  for (var i = 0; i < acctOptions.length; i++) {
+    acctOptions[i].addEventListener("click", function () {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+}
+activeTab(`.top-section .features-section .features-section-wrap .main-features .feature-item`);
